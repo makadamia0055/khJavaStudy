163a0b4892f54c09af361303d732678a718ab05c
makadamia0055 <makend91@naver.com>
2022-10-24 21:29:54 +0900

    day01 files

---
+++ java_GSB/src/day01/HelloWorld.java
@@ -0,0 +1,18 @@
+package day01;
+
+public class HelloWorld {
+
+	// 프로그램이 시작되려면 main이 있어야함.
+	public static void main(String[] args) {
+		System.out.println("Hello world!"); // 이렇게도 가능
+		//한 줄 주석달기
+		/*
+		 * 여러줄 주석 달기
+		 */
+		/** 이거는 기능을 
+		 *  메소드 설명할 때(System.out.println에 마우스 호버하면 텍스트 박스 나오듯
+		 *  설명 나오게 할때 사용(나중에 설명한다 함)
+		 */
+	}
+
+}
---
+++ java_GSB/src/day01/VariableEx01.java
@@ -0,0 +1,39 @@
+package day01;
+
+public class VariableEx01 {
+	public static void main(String[] args) {
+		/* 변수 선언
+		 * 자료형 변수명;
+		 * 자료형 변수명 = 초기값;
+		 *  
+		 */
+		boolean isEven =false; 
+		char operator = '+'; // 무조건 따옴표 ' '
+		int age = 30;
+		float pi = 3.1415f;// float 은 접미사 f/F를 붙여야 한다.
+		double pi2 = 3.1415; // double은 안써도 됨.
+		// 기본 자료형 = 변수에 자료 값이 그대로 저장
+		
+		String str = "Hi"; // 무적권 쌍따옴표 ""
+		// 참조형 = 변수에 자료 값의 번지 값이 저장, heap 에 객체로 자료 저장.
+		
+		//사과의 갯수를 저장하기 위한 변수 선언(변수 명 의미 있게 짓기)
+		int appleNum ;
+		// int appleNum ; 중복된 변수명 선언 불가능(타입이 달라도 불가능) 
+		//학생 평균 성적
+		double stdAvg;
+		
+		int num;
+		int Num; //대소문자를 구분함. 
+		
+		// int int // 예약어는 변수명으로 사용 불가능
+		// 특수문자는 _ $만 가능
+		
+		/* 관례 안지켜도 되지만 많은 개발자들이 지키는 것
+		 * 1. 카멜표기법
+		 * 2. 클래스와 인터페이스는 대문자로 시작
+		 * 3. 상수는 전부 대문자로 
+		 */
+	}
+
+}
