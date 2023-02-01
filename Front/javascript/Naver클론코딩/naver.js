56a63dce2e434a7151ba79530702218b54d973dc
makadamia0055 <makend91@naver.com>
2023-02-01 21:44:46 +0900

    j쿼리 5일찬가?

---
+++ Front/javascript/Naver클론코딩/naver.js
@@ -0,0 +1,108 @@
+$(function(){
+	$('.box-body-right2 .btn-next').click(function(e){
+		e.preventDefault();
+		moveLeft(liRight2, ulRight2, timeRight2);
+	});
+	
+	$('.box-body-right2 .btn-prev').click(function(e){
+		e.preventDefault();
+		moveRight(liRight2, ulRight2, timeRight2);
+	});
+	
+	
+	$('.box-body-right2').hover(function(){
+		clearInterval(rollingRight2);
+	}, function(){
+		rollingRight2 = rollingLeft(rollingObjRigh2.li, rollingObjRigh2.ul, rollingObjRigh2.time, rollingObjRigh2.duration);
+	});
+	
+	$('.box-body-left2 .list-issue').hover(function(){
+		clearInterval(rollingLeft2);
+	}, function(){
+		rollingLeft2 = rollingTop(rollingObjLeft2);
+	});
+	
+	$('.box-body-right4 .box-btn .btn-next').click(function(e){
+		e.preventDefault();
+		let currentNum = $('.box-body-right4 .box-pagination .box-page em').text();
+		if(currentNum==6){
+			currentNum = 6;
+		}else{
+			currentNum++;
+		}
+		$('.box-body-right4 .box-pagination .box-page em').text(currentNum);
+	})
+	$('.box-body-right4 .box-btn .btn-prev').click(function(e){
+		e.preventDefault();
+		let currentNum = $('.box-body-right4 .box-pagination .box-page em').text();
+		if(currentNum==1){
+			currentNum = 1;
+		}else{
+			currentNum--;
+		}
+		$('.box-body-right4 .box-pagination .box-page em').text(currentNum);
+	})
+
+});
+
+let liRight2 = '.box-body-right2 .item-stock';
+let ulRight2 = '.box-body-right2 .list-stock';
+let timeRight2 = 1000;
+let durationRight2 = 2000;
+
+let rollingObjRigh2 = {
+	li : '.box-body-right2 .item-stock',
+ 	ul : '.box-body-right2 .list-stock',
+	time : 1000,
+	duration : 2000
+}
+let rollingObjLeft2 = {
+	liSelector : '.box-body-left2 .item-issue',
+	ulSelector : '.box-body-left2 .list-issue',
+	duration : 2000,
+	animationTime : 1000
+}
+let rollingRight2 = rollingLeft(rollingObjRigh2.li, rollingObjRigh2.ul, rollingObjRigh2.time, rollingObjRigh2.duration);
+let rollingLeft2 = rollingTop(rollingObjLeft2);
+
+function moveLeft(liSelector, ulSelector, time){
+	let width = $(liSelector).first().innerWidth();
+	if(!$(liSelector).first().is(':animated')){
+		$(liSelector).first().animate({
+			marginLeft : `-${width}px`
+		},time, function(){
+			$(this).detach().appendTo(ulSelector).removeAttr('style');
+		});
+	}
+}
+function moveRight(liSelector, ulSelector, time){
+	let width = $(liSelector).first().innerWidth();
+	if(!$(liSelector).first().is(':animated')){
+		$(liSelector)
+			.last()
+			.detach()
+			.prependTo(ulSelector)
+			.css('marginLeft',`-${width}px`)
+			.animate({
+				marginLeft : 0
+			}, time);
+	}
+}
+function rollingLeft(liSelector, ulSelector, animationTime, duration){
+	return setInterval(moveLeft,duration,liSelector, ulSelector, animationTime);
+}
+function moveTop(liSelector, ulSelector, animationTime){
+	let height = $(liSelector).first().innerHeight();
+	if(!$(liSelector).first().is(':animated')){
+		$(liSelector).first().animate({
+				marginTop : `-${height}px`
+			}, animationTime, function(){
+				$(this).detach().appendTo(ulSelector).removeAttr('style');
+			});
+	}
+}
+function rollingTop(rollingObj){
+	return setInterval(moveTop,rollingObj.duration,rollingObj.liSelector, 
+		rollingObj.ulSelector, rollingObj.animationTime);
+}
+
