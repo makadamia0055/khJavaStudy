c3eca204e18a795caef367e9e0dc2891de8a0129
makadamia0055 <makend91@naver.com>
2023-03-03 20:21:56 +0900

    spring 17 게스트 인터셉터 및 멤버 인터셉터 구현

---
+++ spring_project/spring/src/main/java/kr/kh/spring/interceptor/MemberInterceptor.java
@@ -0,0 +1,27 @@
+package kr.kh.spring.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import kr.kh.spring.vo.MemberVO;
+
+public class MemberInterceptor extends HandlerInterceptorAdapter{
+	@Override
+	public boolean preHandle(HttpServletRequest request, 
+			HttpServletResponse response, 
+			Object handler)
+			throws Exception {
+		HttpSession session = request.getSession();
+		MemberVO user = (MemberVO)session.getAttribute("user");
+		
+		//회원이 null이면 메인 페이지로 가게 
+		if(user == null) {
+			response.sendRedirect(request.getContextPath() + "/");
+			return false;
+		}
+		return true;
+	}
+}
